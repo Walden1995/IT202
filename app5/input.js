8e5d2b53da675181eeddca465629363a4efe8827
Walden1995 <swalde3@uic.edu>
2017-10-09 16:43:40 -0500

    made changes to document

--- app5/input.js
+++
@@ -1,9 +0,0 @@
-function openTab(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  $(".tabcontent").hide();
-  $(".tablinks").removeClass("active");
-
-  $('#' + tabName).show();
-  evt.currentTarget.className += " active";
-}
