3520ab47b6c27fbdd897dc638b9469e904db5bdd
Walden1995 <swalde3@uic.edu>
2017-10-12 10:35:30 -0500

    Some "slight"  refactoring

--- github-query.js
+++ github-query.js
@@ -2,21 +2,16 @@ jQuery.githubContent = function(username, repo, callback) {
    jQuery.getJSON('https://api.github.com/repos/'+username+'/'+repo+'/contents?callback=?',callback)
 }
 
-jQuery.fn.loadRepoContent = function(username, repo) {
-    $.githubContent(username, repo, function(data) {
-        var repos = data.data; // JSON Parsing
-
-        $(repos).each(function() {
-            if (this.type == "dir" && this.name != ".vscode") {
-                
-							var node = $("#listitem").clone();
-							
-							node.find("A").attr("href",(this.name) + "/");
-							node.find("STRONG").text(this.name.substring(0,8));
-
-							$("#list").append(node);
-		
-            }
-        });      
-      });
-};
+function loadContent() {
+    $.get("folders.JSON",function(data){
+        $.each(data, function() {
+            var node = $(".sample").clone();
+            node.removeClass("sample");
+            node.find("A").attr("href",(this.name) + "/");
+            node.find("STRONG").text(this.name.substring(0,8));
+            $("#list").append(node); 
+        });
+    });
+}
+    
+        
