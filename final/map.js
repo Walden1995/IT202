614b778546c46c5f186a80c9cfd177104c574e1d
Stephen Walden <swalde3@uic.edu>
2017-11-29 01:27:45 +0000

    fixed nasa map issues... TODO: fix formatting

--- final/map.js
+++ final/map.js
@@ -1,8 +1,8 @@
-function getdata(input){
+function plotNasa(input, lat, long){
   if(input != ""){
     $.get(input,function(response){
       console.log(response);
-      //plotdata(response,map);
+      plotdata(response, initMap(), lat, long);
     });
   }
 }
@@ -10,56 +10,32 @@ function getdata(input){
 function initMap() {
   var uluru = {lat: 41.8781, lng: -87.6298};
   var map = new google.maps.Map(document.getElementById('Map'), {
-    zoom: 10,
+    zoom: 6,
     center: uluru
   });
+  return map;
 }
 
-function plotdata(datatoplot,gmap){
+function plotdata(datatoplot,gmap, lat, long){
+  // gmap.center = {lat: lat, lng: long};
   
-  $("#textout").empty();
-  var div = $("#textout");
-  var i = 0;
-
   $.each(datatoplot,function(index,curr){
-    i++;
-    var card = $("#sample").clone();
-    card.removeClass("tabinactive");    
-    
-    var newpos = {lat: parseFloat(curr.latitude), lng: parseFloat(curr.longitude)};
-    
+    var newpos = {lat: parseFloat(curr.geolocation.coordinates[1]), lng: parseFloat(curr.geolocation.coordinates[0])};
+    console.log(newpos);
     var contentString = '<div id="content">'+
     '<div id="siteNotice">'+
     '</div>';
-    if(curr.results === "Buisness Not Located" || curr.results === "Fail"){
-      contentString = contentString + '<h2 id="firstHeading" class="firstHeading" style="background-color:red">'+curr.inspection_id+'</h1>';
-    } else if (curr.results === "Out of Business" || curr.results === "Not Ready" || curr.results === "No Entry" || curr.results === "Pass w/ Conditions"){
-      contentString = contentString + '<h2 id="firstHeading" class="firstHeading" style="background-color:orange">'+curr.inspection_id+'</h1>';
-    } else {
-      contentString = contentString + '<h2 id="firstHeading" class="firstHeading" style="background-color:green">'+curr.inspection_id+'</h1>';      
+    if(curr.fall === "Fell"){
+      contentString = contentString + '<h2 id="firstHeading" class="firstHeading" style="background-color:red">'+curr.name+'</h1>';
+    } else if (curr.results === "Found"){
+      contentString = contentString + '<h2 id="firstHeading" class="firstHeading" style="background-color:green">'+curr.name+'</h1>';
     }
     
     contentString = contentString + '<div id="bodyContent">'+
-    '<p><b>'+curr.dba_name +' is a '+curr.facility_type +
-    ' located at '+curr.address+'</p><p>Inspected on: '+ curr.inspection_date.split("T",1)[0] +
-    '</p><p>Inspection status: '+ curr.results +
+    '<p><b>'+curr.id +' is a '+curr.recclass +'</p><p>Reported on: '+ curr.year.split("T",1)[0] +
     '</p>'+
     '</div>'+
     '</div>';
-
-    if(curr.results === "Buisness Not Located" || curr.results === "Fail"){
-      card.find("#title").attr("style","background-color:#ff7777");
-    } else if (curr.results === "Out of Business" || curr.results === "Not Ready" || curr.results === "No Entry" || curr.results === "Pass w/ Conditions"){
-      card.find("#title").attr("style","background-color:#ff9933");
-    } else {
-      card.find("#title").attr("style","background-color:#bbff55");
-    }
-
-    card.find("#title").text(curr.inspection_id);
-    card.find("#name").text(curr.dba_name);
-    card.find("#cardinfo").text(curr.facility_type + " at " + curr.address + " had inspection on " + curr.inspection_date.split("T",1)[0]);
-    card.find("#status").text("Status: " + curr.results);
-    div.append(card);
 
     var infowindow = new google.maps.InfoWindow({
       content: contentString
@@ -68,12 +44,11 @@ function plotdata(datatoplot,gmap){
     var marker = new google.maps.Marker({
       position: newpos,
       map: gmap,
-      title: curr.inspection_id + " " + curr.results
+      title: curr.id + " " + curr.fall
     });
     
     marker.addListener('click', function() {
       infowindow.open(gmap, marker);
     });
   });
-  $("#Numcount").text(i + " inspections returned.");
 }
